46955105f31b3bddecae3c2f99f5e6f02867972f
1406205 <1406205@Y350SDP16.ad.dundeeandangus.ac.uk>
2015-12-04 09:54:13 +0000

    01 - Start

---
+++ src/javaapplication005/JavaApplication005.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication005;
+
+/**
+ *
+ * @author 1406205
+ */
+public class JavaApplication005 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
